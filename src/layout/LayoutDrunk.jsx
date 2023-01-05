c84349c95111bdc58432cde7b9a953234298d1d2
kmyobin <sowhat0516@gmail.com>
2023-01-05 22:34:22 +0900

    성적 통지표 제작

--- src/layout/LayoutDrunk.jsx
+++ src/layout/LayoutDrunk.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import styles from './Layout.module.css';
-import { motion } from 'framer-motion';
-import Header from './Header/Header';
+import React from "react";
+import styles from "./Layout.module.css";
+import { motion } from "framer-motion";
+import Header from "./Header/Header";
 
 function LayoutDrunk(props) {
   return (
     <div className={styles.layout}>
-      <Header period="5교시" 
-      name={sessionStorage.getItem("name")}
-      area="금지 영역"
+      <Header
+        period="5교시"
+        name={sessionStorage.getItem("name")}
+        area="금단 영역"
       />
       <main className={styles.main}> {props.children} </main>
     </div>
-  )
+  );
 }
 
-export default LayoutDrunk
+export default LayoutDrunk;
--- src/layout/LayoutResult.jsx
+++ src/layout/LayoutResult.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import HeaderHealth from './Header/HeaderHealth';
+import React from "react";
+import HeaderHealth from "./Header/HeaderHealth";
 import styles from "./Layout.module.css";
 
 function LayoutResult(props) {
   return (
     <div className={styles.layout}>
       <HeaderHealth area="성적 통지표" />
-      <main className={styles.main}> {props.children} </main>
+      <main className={styles.main} style={{ paddingTop: "10px" }}>
+        {props.children}
+      </main>
     </div>
-  )
+  );
 }
 
-export default LayoutResult
+export default LayoutResult;
