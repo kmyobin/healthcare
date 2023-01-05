ecd5a37c5e20862b9dc0437bedbd85ea7baefe0e
kmyobin <sowhat0516@gmail.com>
2023-01-06 01:51:32 +0900

    오타 수정

--- src/page/EatPage.jsx
+++ src/page/EatPage.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import LayoutEat from '../layout/LayoutEat'
-import contents from "../questionBox/questionBoxEat"
-import { motion } from 'framer-motion'
-import QNAConatiner from '../containers/QNA/QNAContainer'
+import React from "react";
+import LayoutEat from "../layout/LayoutEat";
+import contents from "../questionBox/questionBoxEat";
+import { motion } from "framer-motion";
+import QNAConatiner from "../containers/QNA/QNAContainer";
 
 function EatPage() {
   return (
     <motion.div
-    initial={{opacity: 0}}
-    animate={{opacity: 1}}
-    exit={{opacity: 0}}
-    transition={{delay: 0.5}}
-  >
-    <LayoutEat>
-      <QNAConatiner next="/life" questionNum={contents.length} contents={contents} />
-    </LayoutEat>
-  /</motion.div>
-  )
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ delay: 0.5 }}
+    >
+      <LayoutEat>
+        <QNAConatiner
+          next="/life"
+          questionNum={contents.length}
+          contents={contents}
+        />
+      </LayoutEat>
+    </motion.div>
+  );
 }
 
-export default EatPage
+export default EatPage;
